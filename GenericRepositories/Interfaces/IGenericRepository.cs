258b2e5cd204d00fc9c8d84416a7b208b2f7be72
David Rogala <34407302+DaveRogala@users.noreply.github.com>
2026-04-21 10:05:02 -0400

    Create IGenericRepository.cs

---
+++ GenericRepositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace GenericRepositories.Interfaces
+{
+    internal interface IGenericRepository<T,U> : IDisposable
+    where T : class
+    where U : DbContext
+    {
+        Task<T> AddAsync(T entity);
+        T Update(T entity);
+        Task<T?> GetAsync(Guid id);
+        Task<IEnumerable<T>> AllAsync();
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task<T?> FindFirstAsync(Expression<Func<T, bool>> predicate);
+        Task<int> SaveChangesAsync();
+        T Delete(T entity);
+    }
+}
